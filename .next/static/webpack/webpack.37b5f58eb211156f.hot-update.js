7b022282723b99bb0e80a92b75c45cc2afb870ef
Ashraf-ali-2000 <122337346+Ashraf-ali-2000@users.noreply.github.com>
2023-09-12 13:04:01 +0500

    auto update

--- .next/static/webpack/webpack.37b5f58eb211156f.hot-update.js
+++ .next/static/webpack/webpack.37b5f58eb211156f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "b9400b794bf3e7ab"; }
+/******/ 	__webpack_require__.h = function() { return "c8c304dd1d1a0b12"; }
 /******/ }();
 /******/ 
 /******/ }
--- .next/static/webpack/webpack.ba53026d82582f55.hot-update.js
+++ .next/static/webpack/webpack.ba53026d82582f55.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "138ac723d8cb37fc"; }
+/******/ 	__webpack_require__.h = function() { return "37b5f58eb211156f"; }
 /******/ }();
 /******/ 
 /******/ }
